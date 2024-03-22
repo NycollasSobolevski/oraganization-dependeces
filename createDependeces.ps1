436ff972f917d446c207a47b265e08d7ebb11df0
nycollassobolevski <sobolevski.academy@gmail.com>
2024-03-22 10:24:18 -0300

    UPDATE createDependences: added '-New' params to create a dependences by variable

--- createDependeces.ps1
+++ createDependeces.ps1
@@ -19,25 +19,92 @@ if ($args -contains "--ignore") {
     }
 }
 
-foreach ($file in $dirs) {
-
-    if ($ignorated.Count -gt 0 -and $ignorated -contains $file) {
-        Write-Host "$file ignorated";
-        continue; 
-    }
-
-    $file_path = Join-Path -Path $file.DirectoryName -ChildPath $file.Name
-    $file = $file.BaseName
+function Create-By-Model {
+   
+    foreach ($file in $dirs) {
     
-    # ======================= getting file data  =======================
+        if ($ignorated.Count -gt 0 -and $ignorated -contains $file) {
+            Write-Host "$file ignorated"
+            continue  
+        }
     
-    if ($file.Contains("Context")) {
-        continue;
+        $file_path = Join-Path -Path $file.DirectoryName -ChildPath $file.Name
+        $file = $file.BaseName
+        
+        # ======================= creating Domain dependences =======================
+    
+        $domain_path      = Join-Path -Path "./Domain/" -ChildPath $file;
+        $model_path       = Join-Path -Path $domain_path -ChildPath "Models";
+        $repository_path  = Join-Path -Path $domain_path -ChildPath "Repositories";
+        $service_path     = Join-Path -Path $domain_path -ChildPath "Services";
+        
+        #creating Domain sub-folders
+        mkdir $model_path ;
+        mkdir $repository_path ;
+        mkdir $service_path;
+        
+        $model_filename = $file + ".cs";
+        $model_content = Get-Content -Path $file_path;
+        #editing namespace of MODEL
+        if($base_namespace -ne ""){
+            for ($line = 0; $line -lt $model_content.Count; $line ++) {
+                if($model_content[$line].Contains("namespace")) {
+                    $model_content[$line] = "namespace " + $base_namespace + ".Domain.Model;";
+                    break
+                }
+            }
+        }
+        $model_content | Out-File -FilePath "$model_path/$model_filename";
+    
+        $irepository_filename = "I" + $file + "Repository.cs";
+        $repository_content = "//! Implements IRepository here!";
+        $repository_content | Out-File -FilePath "$repository_path/$irepository_filename";
+        
+        $iservice_filename = "I" + $file + "Service.cs"
+        $service_content = "//! Implements Iservice here! ";
+        $service_content | Out-File -FilePath "$service_path/$iservice_filename";
+    
+    
+    
+        # ======================= creating Core dependences =======================
+    
+        $core_path      = Join-Path -Path "./Core/" -ChildPath $file;
+        
+        $classmap_filename = $file + "ClassMap.cs";
+        $repository_filename = $file + "Repository.cs"
+        $service_filename = $file + "Service.cs"
+        
+        $map_path         = Join-Path -Path $core_path -ChildPath "Mapping";
+        $repository_path  = Join-Path -Path $core_path -ChildPath "Repository";
+        $service_path     = Join-Path -Path $core_path -ChildPath "Service";
+        
+        mkdir $map_path ;
+        mkdir $repository_path ;
+        mkdir $service_path;
+    
+        $map_file_path = Join-Path -Path $map_path -ChildPath $classmap_filename;
+        $repo_file_path = Join-Path -Path $repository_path -ChildPath $repository_filename;
+        $service_file_path = Join-Path -Path $service_path -ChildPath $service_filename;
+        
+        $classmap_content = "//! Implements classMapping here!";
+        $classmap_content | Out-File -FilePath $map_file_path;
+    
+        $repository_content = "//! Implements $file Repository here!";
+        $repository_content | Out-File -FilePath $repo_file_path;
+        
+        $service_content = "//! Implements $file Service here!";
+        $service_content | Out-File -FilePath $service_file_path;
+    
+    
     }
+}
 
-    # ======================= creating Domain dependences =======================
+function Create-Dependences-By-Name {
+    param (
+        [string]$Name
+    )
 
-    $domain_path      = Join-Path -Path "./Domain/"  -ChildPath $file;
+    $domain_path      = Join-Path -Path "./Domain/" -ChildPath $Name;
     $model_path       = Join-Path -Path $domain_path -ChildPath "Models";
     $repository_path  = Join-Path -Path $domain_path -ChildPath "Repositories";
     $service_path     = Join-Path -Path $domain_path -ChildPath "Services";
@@ -47,36 +114,30 @@ foreach ($file in $dirs) {
     mkdir $repository_path ;
     mkdir $service_path;
     
-    $model_filename = $file + ".cs";
-    $model_content = Get-Content -Path $file_path;
+    $model_filename = $Name + ".cs";
+    $namespace = ""
+    $classname = "public partial class " + $Name;
+    $model_content = ( 
+        $namespace, "", $classname, "{", "  ","}"
+    )
     #editing namespace of MODEL
-    if($base_namespace -ne ""){
-        for ($line = 0; $line -lt $model_content.Count; $line ++) {
-            if($model_content[$line].Contains("namespace")) {
-                $model_content[$line] = "namespace " + $base_namespace + ".Domain.Model;";
-                break
-            }
-        }
-    }
     $model_content | Out-File -FilePath "$model_path/$model_filename";
 
-    $irepository_filename = "I" + $file + "Repository.cs";
+    $irepository_filename = "I" + $Name + "Repository.cs";
     $repository_content = "//! Implements IRepository here!";
     $repository_content | Out-File -FilePath "$repository_path/$irepository_filename";
     
-    $iservice_filename = "I" + $file + "Service.cs"
+    $iservice_filename = "I" + $Name + "Service.cs"
     $service_content = "//! Implements Iservice here! ";
     $service_content | Out-File -FilePath "$service_path/$iservice_filename";
 
-
-
     # ======================= creating Core dependences =======================
 
-    $core_path      = Join-Path -Path "./Core/" -ChildPath $file;
+    $core_path      = Join-Path -Path "./Core/" -ChildPath $Name;
     
-    $classmap_filename = $file + "ClassMap.cs";
-    $repository_filename = $file + "Repository.cs"
-    $service_filename = $file + "Service.cs"
+    $classmap_filename = $Name + "ClassMap.cs";
+    $repository_filename = $Name + "Repository.cs"
+    $service_filename = $Name + "Service.cs"
     
     $map_path         = Join-Path -Path $core_path -ChildPath "Mapping";
     $repository_path  = Join-Path -Path $core_path -ChildPath "Repository";
@@ -93,11 +154,18 @@ foreach ($file in $dirs) {
     $classmap_content = "//! Implements classMapping here!";
     $classmap_content | Out-File -FilePath $map_file_path;
 
-    $repository_content = "//! Implements $file Repository here!";
+    $repository_content = "//! Implements $Name Repository here!";
     $repository_content | Out-File -FilePath $repo_file_path;
     
-    $service_content = "//! Implements $file Service here!";
+    $service_content = "//! Implements $Name Service here!";
     $service_content | Out-File -FilePath $service_file_path;
+    
+}
 
-
+if( $args -contains "-New" ){
+    $index_new = [array]::IndexOf($args, "-New")
+    Create-Dependences-By-Name $args[$index_new + 1]
+}
+else {
+    Create-By-Model
 }
